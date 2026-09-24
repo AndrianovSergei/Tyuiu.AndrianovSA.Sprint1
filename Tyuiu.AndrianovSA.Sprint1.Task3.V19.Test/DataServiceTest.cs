4f059b5c89f8db9f76da1d335f7b140902fd2a23
Сергей Андрианов <Tyuiu.AndrianovSA@outlook.com>
2026-09-24 14:13:35 +0500

    Task3

---
+++ Tyuiu.AndrianovSA.Sprint1.Task3.V19.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.AndrianovSA.Sprint1.Task3.V19.Lib;
+
+namespace Tyuiu.AndrianovSA.Sprint1.Task3.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            int x1 = 1;
+            int y1 = 1;
+            int x2 = 4;
+            int y2 = 4;
+
+            bool res = ds.ElephCanMove(x1, y1, x2, y2);
+
+            Assert.AreEqual(true, res);
+        }
+    }
+}
---
+++ Tyuiu.AndrianovSA.Sprint1.Task3.V19/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.AndrianovSA.Sprint1.Task3.V19.Lib;
+
+namespace Tyuiu.AndrianovSA.Sprint1.Task3.V19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Андрианов С. А. | ИИПб-26-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                  *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнил: Андрианов Сергей Александрович | ИИПб-26-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая печатает true или false в зависимости от     *");
+            Console.WriteLine("* того, может ли шахматная фигура «Слон» с одного заданного поля          *");
+            Console.WriteLine("* шахматной доски перейти за один ход на другое.                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите координату x1 (1-8): ");
+            int x1 = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Введите координату y1 (1-8): ");
+            int y1 = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Введите координату x2 (1-8): ");
+            int x2 = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Введите координату y2 (1-8): ");
+            int y2 = int.Parse(Console.ReadLine()!);
+
+            bool result = ds.ElephCanMove(x1, y1, x2, y2);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine($"* Слон может совершить ход: {result}                                      *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.ReadKey();
+        }
+    }
+}
